346cc30bf653552bd76e21d404c67de026340f05
utasc <utasc.work@gmail.com>
2024-01-19 12:36:38 +0200

    Класс події для отриманих повідомлень

---
+++ TelegramBotEngineLib/MsgEventArgs.cs
@@ -0,0 +1,29 @@
+using Newtonsoft.Json.Linq;
+using TelegramBotEngineLib.Classes;
+
+namespace TelegramBotEngineLib
+{
+
+    public class MsgEventArgs
+    {
+        public MsgEventArgs(JToken msg )
+        {
+            MassageJToken = msg;
+        }
+
+        public JToken MassageJToken { get;private set; }
+
+        messageT msg=null;
+        public messageT MassageClass 
+        { 
+            get 
+            {
+                if(msg==null)
+                msg=MassageJToken.ToObject<messageT>();
+                return msg;
+            }
+        }
+
+        public string MassageString{get => MassageJToken.ToString();}
+    }
+}
