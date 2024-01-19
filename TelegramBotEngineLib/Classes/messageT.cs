a0046566faee4199e567e6d15b2897331cf11967
utasc <utasc.work@gmail.com>
2024-01-19 12:35:47 +0200

    створено базові класи для читання повідомлень

---
+++ TelegramBotEngineLib/Classes/messageT.cs
@@ -0,0 +1,11 @@
+
+namespace TelegramBotEngineLib.Classes
+{
+    public class messageT{
+      public int message_id;
+      public fromT from;
+      public chatT chat;
+      public long date;
+      public string text;
+    }
+}
